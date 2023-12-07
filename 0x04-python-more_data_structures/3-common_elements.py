1c087aac51baa07d93de7e039aadad2266e1425b
ManLikeMarko <cramohchalo@gmail.com>
2023-12-07 02:33:27 +0000

    corrected 3

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,11 +2,11 @@
 def common_elements(set_1, set_2):
     """
     returns a set of common elements in two sets
-    
+
     Args:
     set_1: first set
     set_2: second set
-    
+
     Returns:
     set of common elements
     """
