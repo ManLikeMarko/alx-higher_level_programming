8c3f12f1d8e35eaa9a4c6e5f8a9a35fdafdbcdf1
ManLikeMarko <cramohchalo@gmail.com>
2023-12-19 13:21:39 +0000

    2-square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+class Square:
+    """Defines square"""
+
+    def __init__(self, size=0):
+        """
+        Constructor
+        Args:
+            size: length of one size
+        Raises:
+            ValueError: if size is less than 0
+            TypeError: if size is not int
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """
+            Area of square
+            Return: Size squared
+            """
+            return self.__size ** 2
